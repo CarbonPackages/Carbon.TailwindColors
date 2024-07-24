5284e7d11c0e9ba15e9a174e40cf2f448da32c6e
Jon Uhlmann <account@uhlmann.pro>
2024-07-24 15:43:23 +0200

    Fix: Text overflow

--- Resources/Private/Editor/Component/Colors/PreviewBox.tsx
+++ Resources/Private/Editor/Component/Colors/PreviewBox.tsx
@@ -16,7 +16,7 @@ const styles = stylex.create({
         paddingInline: sizes.spacingHalf,
         gap: sizes.spacingHalf,
         overflow: "hidden",
-        textEllipsis: "ellipsis",
+        textOverflow: "ellipsis",
     },
     checkboard: {
         backgroundImage: `url('data:image/svg+xml, <svg xmlns="http://www.w3.org/2000/svg" width="400" height="400" fill-opacity=".25"><rect x="200" width="200" height="200" /><rect y="200" width="200" height="200" /></svg>')`,
